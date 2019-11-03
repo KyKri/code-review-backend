800362a069ed9b8a7702e94344608d8bfa282b59
Kyle Krick <krkrick@gmail.com>
2019-11-02 17:45:04 -0700

    added Algorithm model for mongodb

---
+++ models/Algorithm.js
@@ -0,0 +1,22 @@
+let mongoose = require('mongoose');
+
+// import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+let Algorithm = new Schema({
+    name: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    runtimeComplexities: {
+        type: Object
+    },
+    spaceComplexity: {
+        type: String
+    }
+});
+
+export default mongoose.model('Algorithm', Algorithm);
