4a693564377dad884f9f3959026f6d541b34b972
Kyle Krick <krkrick@gmail.com>
2019-11-02 17:44:49 -0700

    added basic config and set up for mongodb

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 let express = require('express');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://codereview/algorithms');
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("Database connection successful.");
 });
+
+app.use('/', router);
 
 app.listen(4000, () => {
     console.log('Express server running on port 4000.')
