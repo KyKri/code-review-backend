c9cebb807f7db786aa680ab124136f796d6e2ffd
Kyle Krick <krkrick@gmail.com>
2019-11-03 16:48:18 -0800

    updated to take advantage of babel for imports

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-let express = require('express');
-let cors = require('cors');
-let bodyParser = require('body-parser');
-let mongoose = require('mongoose');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const router = express.Router();
@@ -9,16 +9,14 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://codereview/algorithms');
+mongoose.connect('mongodb://[server]/issues');
 
 const connection = mongoose.connection;
 
 connection.once('open', () => {
-    console.log("Database connection successful.");
+    console.log('MongoDB database connection established successfully!');
 });
 
 app.use('/', router);
 
-app.listen(4000, () => {
-    console.log('Express server running on port 4000.')
-});
+app.listen(4000, () => console.log(`Express server running on port 4000`));
