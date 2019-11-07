cddb5cceb6869da9aed2dcea0e206a6f53466fa2
Kyle Krick <krkrick@gmail.com>
2019-11-06 19:55:18 -0800

    added endpoints add, update, delete

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 import Algorithm from './models/Algorithm';
+import { runInNewContext } from 'vm';
 
 const app = express();
 const router = express.Router();
@@ -37,6 +38,51 @@ router.route('/algorithms/:id').get((req, res) => {
         }
         else {
             res.json(algorithm);
+        }
+    });
+});
+
+router.route('/algorithms/add').post((req, res) => {
+    let algorithm = new Algorithm(req.body);
+
+    algorithm.save()
+        .then(algorithm => {
+            res.status(200).json({'message': 'Added successfully.'});
+        })
+        .catch(err => {
+            res.json(400).json({'message': 'Failed to create new record.'});
+        });
+});
+
+router.route('/algorithms/update/:id').post((req, res) => {
+    Algorithm.findById(req.params.id, (err, algorithm) => {
+        if (!algorithm) {
+            return next(new Error('Could not load document.'));
+        }
+        else {
+            algorithm.name = req.body.name;
+            algorithm.description = req.body.description;
+            algorithm.runtimeComplexities = req.body.runtimeComplexities;
+            algorithm.spaceComplexity = req.body.spaceComplexity;
+
+            algorithm.save()
+                .then(algorithm => {
+                    res.json({'message': 'Updated successfully.'});
+                })
+                .catch(err => {
+                    res.status(400).json({'message': 'Failed to update record.'});
+                });
+        }
+    });
+});
+
+router.route('/algorithms/delete/:id').post((req, res) => {
+    Algorithm.findByIdAndRemove({_id: req.params.id}, (err, algorithm) => {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json({'message': 'Deleted successfully.'});
         }
     });
 });
